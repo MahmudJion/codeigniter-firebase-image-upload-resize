3bfd3f1da362543cf495de68345bb4fd20de6f7d
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2023-04-26 10:44:18 +0100

    update FileUpload.php

--- app/Controllers/FileUpload.php
+++ app/Controllers/FileUpload.php
@@ -2,6 +2,8 @@
 namespace App\Controllers;
 
 use CodeIgniter\Controller;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
 
 class FileUpload extends Controller
 {
@@ -15,9 +17,6 @@ class FileUpload extends Controller
     {
         helper(['form', 'url']);
 
-        $database = \Config\Database::connect();
-        $db = $database->table('users');
-
         $input = $this->validate([
             'file' => [
                 'uploaded[file]',
@@ -30,15 +29,45 @@ class FileUpload extends Controller
             print_r('Choose a valid file');
         } else {
             $img = $this->request->getFile('file');
-            $img->move(WRITEPATH . 'uploads');
 
-            $data = [
-                'name' =>  $img->getName(),
-                'type'  => $img->getClientMimeType()
+            // Initialize Firebase Storage
+            $serviceAccount = ServiceAccount::fromJsonFile(APPPATH . 'FirebaseCredentials.json');
+            $firebase = (new Factory)
+                ->withServiceAccount($serviceAccount)
+                ->create();
+            $storage = $firebase->getStorage();
+
+            // Upload the original image to Firebase Storage
+            $originalImagePath = WRITEPATH . 'uploads/' . $img->getName();
+            $imageData = file_get_contents($originalImagePath);
+            $storage->getBucket()->upload($imageData, [
+                'name' => 'images/' . $img->getName()
+            ]);
+
+            // Resize the image using CodeIgniter's Image Manipulation library
+            $config = [
+                'image_library' => 'gd2',
+                'source_image' => $originalImagePath,
+                'create_thumb' => false,
+                'maintain_ratio' => true,
+                'width' => 800,
+                'height' => 800
             ];
+            $this->load->library('image_lib', $config);
+            $this->image_lib->resize();
 
-            $save = $db->insert($data);
-            print_r('File has been successfully uploaded');
+            // Upload the resized image to Firebase Storage
+            $resizedImagePath = WRITEPATH . 'uploads/' . 'thumb_' . $img->getName();
+            $resizedImageData = file_get_contents($resizedImagePath);
+            $storage->getBucket()->upload($resizedImageData, [
+                'name' => 'images/thumb_' . $img->getName()
+            ]);
+
+            // Delete the original and resized images from local server
+            unlink($originalImagePath);
+            unlink($resizedImagePath);
+
+            print_r('File has been successfully uploaded and resized');
         }
     }
 }
